0ea380887a4b64861f2ce457c934d0733352b486
Aakash Bhatnagar <Aakash Bhatnagar>
2016-05-14 15:53:01 +0530

    Inital Commit (Images loaded from json)

---
+++ app/src/main/java/com/example/aakash/imagemultiple/MarvelModel.java
@@ -0,0 +1,15 @@
+package com.example.aakash.imagemultiple;
+
+public class MarvelModel{
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    String url;
+
+}
